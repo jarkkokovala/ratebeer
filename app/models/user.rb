d4a486d34b57d0c8da138350e3b86278860d59ba
Jarkko Kovala <jarkko.kovala@iki.fi>
2018-09-20 17:50:23 +0300

    Hienosäätöä käyttäjiin ja niiden käsittelyyn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beerclubs, through: :memberships
 
+  has_secure_password
+
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+
+  PASSWORD_FORMAT = /\A
+  (?=.{4,})          # Must contain 4 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[A-Z])        # Must contain an upper case character
+  /x
+
+  validates :password, format: { with: PASSWORD_FORMAT }
 
   include RatingAverage
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_101804) do
+ActiveRecord::Schema.define(version: 2018_09_20_111530) do
 
   create_table "beerclubs", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_09_20_101804) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
